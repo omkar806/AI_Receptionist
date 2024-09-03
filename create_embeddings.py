0e2aa2a6117f658a84faebbd1fb9d951ebf8618a
Omkar Malpure <77787482+omkar806@users.noreply.github.com>
2024-09-03 20:05:16 +0530

    Delete create_embeddings.py

--- create_embeddings.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-sentences = "This is an example sentence"
-
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-# embeddings = model.encode(sentences)
-# print(embeddings)
-def create_embeddings(sentence:str):
-    embeddings = model.encode(sentences=sentence)
-    return embeddings
